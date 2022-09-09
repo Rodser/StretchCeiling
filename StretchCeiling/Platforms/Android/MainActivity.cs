870b51275ec459dc60abc18cd1cd13f2c3825d6b
Rodser <rodser@ro.ru>
2022-09-09 09:50:20 +0400

    statusbar color

--- StretchCeiling/Platforms/Android/MainActivity.cs
+++ StretchCeiling/Platforms/Android/MainActivity.cs
@@ -7,4 +7,9 @@ namespace StretchCeiling;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle bundle)
+    {        
+        base.OnCreate(bundle);
+        Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+    }
 }
