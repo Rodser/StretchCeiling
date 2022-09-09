517fa008a7a0d1bfed4f8086247e392e5cc6a173
Rodser <rodser@ro.ru>
2022-09-09 14:21:28 +0400

    update total price

--- StretchCeiling/ViewModel/MainViewModel.cs
+++ StretchCeiling/ViewModel/MainViewModel.cs
@@ -1,26 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.ApplicationModel.DataTransfer;
 using StretchCeiling.Model;
 using StretchCeiling.View.Pages;
 using System.Collections.ObjectModel;
 
 namespace StretchCeiling.ViewModel
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject, IQueryAttributable
     {
+        private double _price;
+
+        [ObservableProperty] private ObservableCollection<Ceiling> _ceilings;
+
+        public double Price { get => _price; set { _price = value; SetProperty(ref _price, value); }  }
+
         public MainViewModel()
         {
             _ceilings = AppShell.CeilingSerxice.GetCeilings();
-            _price = AppShell.CeilingSerxice.TotalPrice;
+            // _price = AppShell.CeilingSerxice.TotalPrice;
         }
-
-        [ObservableProperty] private ObservableCollection<Ceiling> _ceilings;
-        [ObservableProperty] private double _price;
 
         [RelayCommand]
         private async Task BuildCeiling()
         {
             await Shell.Current.GoToAsync(nameof(BuilderPage));
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("updated"))
+            {
+                bool updated = (bool)query["updated"];
+                if (updated)
+                {
+                    Price = AppShell.CeilingSerxice.TotalPrice; ;
+                }
+            }
         }
 
         //[RelayCommand]
@@ -38,7 +54,7 @@ namespace StretchCeiling.ViewModel
         //[RelayCommand]
         //private void Delete(string s)
         //{
-            
+
         //}
     }
 }
